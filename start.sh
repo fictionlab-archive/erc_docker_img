46c76f48cae52af720af8e5cd722635bf4b393b5
Błażej Sowa <bsowa123@gmail.com>
2020-08-26 15:43:47 +0200

    Run erc_bringup.launch at start

--- start.sh
+++ start.sh
@@ -6,4 +6,8 @@ echo "source /catkin_ws/devel/setup.bash" >> /etc/bash.bashrc
 echo "ROS_IP=${ROS_IP}" >> /etc/environment
 echo "ROS_MASTER_URI=${ROS_MASTER_URI}" >> /etc/environment
 
-python /root/.local/lib/python2.7/site-packages/freedomrobotics/agent.py
+python /root/.local/lib/python2.7/site-packages/freedomrobotics/agent.py &
+
+roslaunch erc_example erc_bringup.launch
+
+fg %1
